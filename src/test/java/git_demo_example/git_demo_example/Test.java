30d4282f6b9fdd9964151b14b3f4ce408e446cf7
DESKTOP-3KTC6IA\aksha <vishalwayalkar2018@gmail.com>
2024-05-30 20:46:06 +0530

    Test updated with fun2

--- src/test/java/git_demo_example/git_demo_example/Test.java
+++ src/test/java/git_demo_example/git_demo_example/Test.java
@@ -3,5 +3,6 @@ package git_demo_example.git_demo_example;
 public class Test {
   public static void main(String args[]){
     System.out.println("Hello World");
+    System.out.println("sdfgh");
   }
 }
