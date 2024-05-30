70da4495e57d5b60ad762864730fe0c5f5e87861
Vishal Wayalkar <87711998+vishalwayalkar@users.noreply.github.com>
2024-05-30 20:33:33 +0530

    Update Test1.java

--- src/test/java/git_demo_example/git_demo_example/Test1.java
+++ src/test/java/git_demo_example/git_demo_example/Test1.java
@@ -2,6 +2,6 @@ package git_demo_example.git_demo_example;
 
 public class Test1 {
   public static void main(String args[]){
-    System.out.println();
+    System.out.println("Hello World");
   }
 }
