1735dace2eea7ea193be9da178fd4e9ff832df07
DESKTOP-3KTC6IA\aksha <vishalwayalkar2018@gmail.com>
2024-05-30 20:14:14 +0530

    initial commit

---
+++ src/main/java/git_demo_example/git_demo_example/App.java
@@ -0,0 +1,13 @@
+package git_demo_example.git_demo_example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
